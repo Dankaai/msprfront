e3d4a85cd58e5e5c2844a968ab87cd54bbb32dc5
darmex59 <htaviaux@gmail.com>
2020-04-21 12:08:33 +0200

    initialisation du dossier plugin gestion-festival

---
+++ wp-content/plugins/gestion-festival/gestion-festival.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package gestion-festival
+ * @version 1.7.2
+ */
+/*
+Plugin Name: Gestion-Festival
+Plugin URI: http://msprfront/
+Description: plugin de la gestion du festival avec additio,, suppressio, modification des artistes, concerts, scene, stands.
+Author: Hugo Taviaux
+Version: 1.0
+Author URI: http://msprfront/
+*/
